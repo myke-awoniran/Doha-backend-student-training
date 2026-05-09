5961913e9bd28d09278d867334a7dbde272c136a
Saeed-60307149 <darabdullah004@gmail.com>
2026-05-09 04:29:19 +0300

    fix: return full user object directly from signup

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -55,7 +55,7 @@ class AuthService {
             data: {
                 accessToken,
                 refreshToken,
-                user: {id: user.id, email: user.email, firstName: user.firstName, lastName: user.lastName},
+                user,
             },
         };
     }
